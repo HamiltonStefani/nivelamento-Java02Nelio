f385d74cec35616a6ba4882f167dbb1f957a24cb
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2022-12-25 19:43:34 -0300

    Add files via upload

---
+++ src/exercicio02/exerciciosatividade.java
@@ -0,0 +1,32 @@
+package exercicio02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exerciciosatividade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		double largura = sc.nextDouble();
+		
+		double comprimento = sc.nextDouble();
+		
+		double metroQuadrado  = sc.nextDouble();
+		
+		double area = largura * comprimento;
+		double preco  = area * metroQuadrado;
+		
+		
+		System.out.printf("ÁREA = %.2f\n", area);
+		System.out.printf("PREÇO = %.2f\n", preco);
+		
+		
+		sc.close();
+		
+	}
+
+}
